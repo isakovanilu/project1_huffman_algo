d575b9ba987ca36573b711f742f76db043dea432
Nilu Isakova <niluisakova@gmail.com>
2024-04-21 15:25:39 -0500

    read input file and count words

--- compressor.py
+++ compressor.py
@@ -7,10 +7,8 @@ def read_input(input_file):
         # splitting the data into separate lines
         # using the split() function
         lines = input_data.split()
-        
         # create a counter object to count occurrences of each word
         word_count = Counter(lines)
-    
         # convert the counter object to a dictionary
         word_count_dict = dict(word_count)
         print(word_count_dict)
