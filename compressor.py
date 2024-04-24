f2cceaf83a6ef5957c592ed3e01d3b0efa1749ee
Nilu Isakova <niluisakova@gmail.com>
2024-04-24 13:38:07 -0500

    calculate frequencies

--- compressor.py
+++ compressor.py
@@ -1,16 +1,13 @@
-from collections import Counter
 
-
-def read_input(input_file):
+def calculate_frequencies(input_file):
     with open(input_file,'r') as file:
-        input_data = file.read()
-        # splitting the data into separate lines
-        # using the split() function
-        lines = input_data.split()
-        # create a counter object to count occurrences of each word
-        word_count = Counter(lines)
-        # convert the counter object to a dictionary
-        word_count_dict = dict(word_count)
-        print(word_count_dict)
-        return word_count_dict
-read_input('input.txt')
+        text = file.read()
+    freq = {}
+    for char in text:
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+    return freq
+
+print(calculate_frequencies('input.txt'))
